0cd44939a9852061484bedb7e0891b30fdf03a01
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2019-04-08 10:29:25 +0200

    playbooks, conf and readme

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ Ansible-based DBaaS automation for Openstack projects.
 ### Infrastructure
 
 These playbooks are used to deploy and manage persistant-storage-based workloads:
+
 * in order to provide high-availability and fencing, pacemaker is used ;
 * in order to manage the worklodas, nomad is used.
 
 These playbooks also provide a control plane:
+
 * consul & nomad masters.
 
 ### Security
@@ -21,6 +23,7 @@ The user cannot login as administrator from outside of the cluster.
 ### Provisionning
 
 Each database is hosted on a dedicated pacemaker cluster. On the cluster side, a resource group is created:
+
 * the cinder volume ;
 * the mountpoints ;
 * the floating IP ;
@@ -28,6 +31,7 @@ Each database is hosted on a dedicated pacemaker cluster. On the cluster side, a
 * the nomad agent.
 
 Some nomad tasks are created:
+
 * a service task to run the database engine ;
 * a batch task to configure the database (users, schemas…).
 
@@ -39,20 +43,48 @@ When the user wants to run some SQL administration queries, a nomad batch task 
 
 ### The configuration files
 
+```bash
+# Copy the configuration files to a git-ops repo
+export GITOPS=~/my-gitops-repo
+export BRANCH=my-branch
+cd $GITOPS
+git checkout -b $BRANCH
+cd -
+mkdir -p $GITOPS/{etc,files}
+cp example/*.yml $GITOPS/etc
+cp example/*.txt $GITOPS/etc
+# Create an openrc file
+vi $GITOPS/files/openrc.sh
+# Update the repo
+cd $GITOPS
+git add *
+git commit -a -m "First commit to branch $BRANCH"
+git push
+```
+
 ### The commands to run
 
 ```bash
+export DBAAS_PLAYBOOK=../cloud-dbaas-ansible
 # Create the control-plane
-ansible-playbook control-plane.yml -e@etc/control_plane.yml
+ansible-playbook $DBAAS_PLAYBOOK/control-plane.yml -e@etc/control_plane.yml
 ```
 The HEAT stacks are deployed.
+
 `hosts.ini` has been written and provides Nomad's endpoint address.
 
 ```bash
 # Create a PostgreSQL database
-ansible-playbook db.yml \
+ansible-playbook $DBAAS_PLAYBOOK/database.yml \
   -e type=postgresql \
   -e db_name=my-first-db \
   -e db_size_gb=10
 ```
 `my-first-db.yml` has been written and contains how to connect against the database.
+
+```bash
+# Add the new database to gitops
+git add my-first-db.yml
+git commit my-first-db.yml -m "New DB my-first-db"
+git push
+```
---
+++ autoconf.sh
@@ -0,0 +1,99 @@
+for f in $REPO_PATH/files/*.pem ; do
+	export OS_CACERT=$f
+done
+
+for f in $REPO_PATH/files/*openrc* ; do
+	export OPENRC_FILE=$f
+	source $f
+done
+
+export CONTROLPLANE_NODES_NUMBER=$(openstack server list --name controlplane | grep -ccontrolplane)
+
+export ANSIBLE_PLAYBOOK_OPTS=" \
+	/etc/ansible/roles/cloud-dbaas-ansible/autoconf_controlplane.yml \
+	-e@$ETC_PATH/control_plane.yml \
+	-e monitoring_consul_cluster={{ansible_default_ipv4.address}} \
+	-e scheduler_bootstrap_expect=$CONTROLPLANE_NODES_NUMBER \
+	-e monitoring_consul_ui=false \
+	-e openstack_cluster_node_openrc_source=$OPENRC_FILE \
+	-e openstack_cluster_node_ca_cert_source=$OS_CACERT"
+
+# Master auto-conf
+if echo $HOSTNAME | grep -q controlplane ; then
+	ansible-playbook $ANSIBLE_PLAYBOOK_OPTS -t os-ready,controlplane \
+		-e monitoring_server_mode=true \
+		-e scheduler_config_consul=true \
+		-e scheduler_server_mode=true \
+		-e dnsmasq_listening_interfaces=''
+fi
+
+# LB auto-conf
+if echo $HOSTNAME | grep -q lb ; then
+	source /etc/ansible/roles/cloud-exploitation-ansible/files/generate_local_facts.sh lb
+
+	export PUBLIC_IP=$(openstack stack show ip -c outputs -f yaml \
+		| grep -A 1 publication_floating_ip_id \
+		| awk '/output_value:/ {print $NF}')
+	
+	export CORE_VOLUME=$( openstack volume list -f value \
+		| fgrep $HOSTNAME \
+		| awk '/core/ {print $1}')
+
+	export ANSIBLE_PLAYBOOK_OPTS="$ANSIBLE_PLAYBOOK_OPTS \
+		-e cluster_node_attribute_value=lb \
+		-e publication_floating_ip_id=$PUBLIC_IP \
+		-e monitoring_consul_ui=true \
+		-e dnsmasq_listening_interfaces=''"
+
+	ansible-playbook $ANSIBLE_PLAYBOOK_OPTS -t os-ready,pacemaker,lb
+
+	sleep 120
+
+	crm_resource --cleanup --resource traefik-service
+	crm_resource --cleanup --resource publication-ip
+	
+	if ! /usr/local/bin/consul members | grep -q server ; then
+		crm_resource --restart --resource consul-service
+	fi
+	
+	sleep 30
+	crm_resource --cleanup --resource consul-info
+fi
+
+# DB auto-conf
+if echo $HOSTNAME | grep -q db ; then
+	source /etc/ansible/roles/cloud-exploitation-ansible/files/generate_local_facts.sh db
+	
+	export DOCKER_VOLUME=$(openstack volume list -f value \
+		| fgrep $HOSTNAME \
+		| awk '/docker/ {print $1}')
+	
+	export CORE_VOLUME=$(openstack volume list -f value \
+		| fgrep $HOSTNAME \
+		| awk '/core/ {print $1}')	
+
+	export ANSIBLE_PLAYBOOK_OPTS="$ANSIBLE_PLAYBOOK_OPTS \
+		-e cluster_node_attribute_value=$DB_NAME"
+
+	if lsb_release -d | grep -qi ubuntu ; then
+		export ANSIBLE_PLAYBOOK_OPTS="$ANSIBLE_PLAYBOOK_OPTS \
+			-e ha_storage_openstack_cloud_info_resource=c-cloud-info"
+	fi
+
+	ansible-playbook $ANSIBLE_PLAYBOOK_OPTS -t os-ready
+	ansible-playbook $ANSIBLE_PLAYBOOK_OPTS -t node \
+		-e 'docker_options="--dns {{ansible_docker0.ipv4.address}}"'
+	ansible-playbook $ANSIBLE_PLAYBOOK_OPTS -t pacemaker
+	sleep 20
+	crm_resource --cleanup
+
+	if ! /usr/local/bin/consul members | grep -q server ; then
+		crm_resource --restart --resource consul-service
+	fi
+
+fi
+
+# Bastion auto-conf
+if echo $HOSTNAME | grep -q bastion ; then
+	ansible-playbook $ANSIBLE_PLAYBOOK_OPTS -t os-ready
+fi
