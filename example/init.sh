d2013d00b4e33172d9f9ddd60c272b2df21cdbd8
mathieu.grzybek <mathieu.grzybek@dgfip.finances.gouv.fr>
2019-04-09 10:23:25 +0200

    Autoconf management

---
+++ example/init.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+sync_git() {
+	git clone $1 $2 || sleep 5
+	test -e $2 || git clone $1 $2
+}
+
+# Create folders to clone roles (one folder per source repo)
+for repo in github ; do
+	mkdir -p /etc/ansible/roles/$repo
+done
+
+# Create local facts folder
+mkdir -p /etc/ansible/facts.d
+
+# Get the required roles
+for l in $(cat $ANSIBLE_REQUIREMENTS_FILE) ; do
+	export src=$(echo $l | awk -F, '{print $1}')
+	export dest=$(echo $l | awk -F, '{print $2}')
+	export branch=$(echo $l | awk -F, '{print $3}')
+	
+	export git_clone="git clone -b $branch $src $dest"
+	
+	$git_clone || sleep 5
+	test -e $dest || $git_clone
+done
+
+. /etc/ansible/roles/github/cloud-dbaas-ansible/autoconf.sh
