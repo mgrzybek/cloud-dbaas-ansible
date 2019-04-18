089d950df3a51d553ddf77a46a2a0d0632e9c021
Mathieu Grzybek <mathieu@grzybek.fr>
2019-04-18 15:03:42 +0200

    Adds a header in generate_local_facts.sh

--- files/generate_local_facts.sh
+++ files/generate_local_facts.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
-
-#if [ -z $OS_CACERT ] ; then
-#    for f in $REPO_PATH/files/*.pem ; do
-#        export OS_CACERT=$f
-#    done
-#fi
+#
+# Build a local Ansible facts file using Openstack API.
+# This is used by ansible-ha-cluster playbook.
+# Environment variables should be present.
+#
 
 openstack server list -f json -c Name -c Networks --name $1 \
 	| perl -pe 's/(\w|-)+=//g ; s/N/n/ ; s/networks/ring0_address/ ; s/, .+?"/"/' \
 	> /etc/ansible/facts.d/cluster_nodes.fact
+
